a38b8c132d19909de123ca7368886a80a40105e7
aravindsuram <aravindersuram@gmail.com>
2020-04-28 01:55:26 +0530

    daily emp wage

--- Employeewage/empwage.sh
+++ Employeewage/empwage.sh
@@ -1,10 +1,14 @@
 present=1
 randomcheck=$((RANDOM%2))
+emprateperhr=20
+emphrs=8
 
 
 if [ $present == $randomcheck ]
 then
-      echo "emp is present"
+        echo "salary=$(($emphrs*$emprateperhr))"
+        echo "emp is present" 
 else
-      echo "emp is abscent"
+       echo "salary=0"
+        echo "emp is abscent"
 fi
